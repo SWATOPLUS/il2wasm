53040dbc22351c21e310482cbff48f4da5e606b1
Pavel Zhdanovich <swatoplus@gmail.com>
2019-03-18 14:07:10 +0300

    Added Primes test for js/cs/wasm

---
+++ MyInterLibrary/InterTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace MyInterLibrary
+{
+    public static class InterTest
+    {
+        public static int RunComputation(int myArg)
+        {
+            var result = GetNthPrime(myArg);
+            Console.WriteLine(result); // To show we can call from AOT code into interpreted code
+            return result;
+        }
+
+        static int GetNthPrime(int n)
+        {
+            var primeCalculator = new PrimeCalculator();
+
+            for (var i = 0; i < n - 1; i++)
+            {
+                primeCalculator.ComputeNextPrime();
+            }
+
+            return primeCalculator.ComputeNextPrime();
+        }
+    }
+
+    class PrimeCalculator
+    {
+        private int current = 1;
+
+        public int ComputeNextPrime()
+        {
+            do
+            {
+                current++;
+            }
+            while (!IsPrime(current));
+
+            return current;
+        }
+
+        static bool IsPrime(int value)
+        {
+            for (var possibleDivisor = 2; possibleDivisor * possibleDivisor <= value; possibleDivisor++)
+            {
+                if ((value % possibleDivisor) == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ samples/BlazorStandalone/wwwroot/primes.js
@@ -0,0 +1,50 @@
+﻿var Primes = { RunComputation: null };
+
+(function (Primes) {
+
+	function IsPrime(value)
+	{
+		for (var possibleDivisor = 2; possibleDivisor * possibleDivisor <= value; possibleDivisor++) {
+			if ((value % possibleDivisor) === 0) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+    function BuildPrimeCalculator() {
+        var current = 1;
+
+        function ComputeNextPrime() {
+            do {
+                current++;
+            }
+            while (!IsPrime(current));
+
+            return current;
+        }
+
+        return { ComputeNextPrime };
+    }
+
+    function GetNthPrime(n) {
+        var primeCalculator = BuildPrimeCalculator();
+
+        for (var i = 0; i < n - 1; i++) {
+            primeCalculator.ComputeNextPrime();
+        }
+
+        return primeCalculator.ComputeNextPrime();
+    }
+
+    function run(n) {
+        var res = GetNthPrime(n);
+        console.log(res);
+        return res;
+    }
+
+
+
+    Primes.RunComputation = run;
+})(Primes);
